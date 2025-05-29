9544795e00fc82ee326a75aa6447400346772a20
Datmos <datmos.tec@gmail.com>
2025-05-29 08:29:05 +0200

    instalacion de addon

--- roverrecipes-addon/run.sh
+++ roverrecipes-addon/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 cd /app/app
+DB_NAME="$DB_NAME"
+DB_USER="$DB_USER"
+DB_PASSWORD="$DB_PASSWORD"
+
+# Usa estas variables para iniciar tu base de datos o aplicación
+echo "Base de datos: $DB_NAME"
+echo "Usuario: $DB_USER"
+echo "Contraseña: $DB_PASSWORD"
+
 exec uvicorn main:app --host 0.0.0.0 --port 8000 
